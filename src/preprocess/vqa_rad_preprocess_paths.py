d428d5dcf2fd951f8daeac53bff4b277d6179ea3
caghankoksal <andrei.mancu@tum.de>
2022-06-16 16:30:15 +0000

    Add image path to vqa pickle samples

--- src/preprocess/vqa_rad_preprocess_paths.py
+++ src/preprocess/vqa_rad_preprocess_paths.py
@@ -9,16 +9,19 @@ from multiprocessing import Process, Manager
 
 dataset_path = Path('/home/mlmi-matthias/VQA-RAD/')
 
-
 with open(dataset_path / 'VQA-RAD_public.json', 'r') as f:
     data = json.load(f)
 
 result = []
 
-for sample in data:
+for sample in tqdm(data):
     if not(sample['image_organ'] == 'CHEST'):
         continue
+    image_path = str(dataset_path / 'images' / sample['image_name'])
+    sample['image_path'] = image_path
     result.append(sample)
+
+print(f'Result length: {len(result)}')
 
 output_path = Path('/home/mlmi-matthias/Data')
 with open(output_path / 'vqa_rad_chest_xrays.pkl', 'wb') as f:
